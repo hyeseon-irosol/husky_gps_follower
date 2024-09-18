20f49a8556317e304e4b513e9c9a907cc01fc783
Hyeseon Lee <145497062+hyeseon-irosol@users.noreply.github.com>
2024-09-18 12:57:01 -0400

    workspace update

--- build/clearpath_common/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/vupadhye/git_repo/husky_gps_follower/src/clearpath_common/clearpath_common
-# Build directory: /home/vupadhye/git_repo/husky_gps_follower/build/clearpath_common
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/clearpath_common/colcon_command_prefix_build.sh
+++
@@ -1,8 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/vupadhye/git_repo/husky_gps_follower/install/clearpath_control/share/clearpath_control/package.sh"
-. "/home/vupadhye/git_repo/husky_gps_follower/install/clearpath_mounts_description/share/clearpath_mounts_description/package.sh"
-. "/home/vupadhye/git_repo/husky_gps_follower/install/clearpath_platform/share/clearpath_platform/package.sh"
-. "/home/vupadhye/git_repo/husky_gps_follower/install/clearpath_platform_description/share/clearpath_platform_description/package.sh"
-. "/home/vupadhye/git_repo/husky_gps_follower/install/clearpath_sensors_description/share/clearpath_sensors_description/package.sh"
-. "/home/vupadhye/git_repo/husky_gps_follower/install/clearpath_description/share/clearpath_description/package.sh"
-. "/home/vupadhye/git_repo/husky_gps_follower/install/clearpath_generator_common/share/clearpath_generator_common/package.sh"
